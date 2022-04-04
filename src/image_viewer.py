b7a5ba9beff23129fed375022d901a6822b39cbf
skiteta <61131444+skiteta@users.noreply.github.com>
2022-04-04 18:54:29 +0900

    add some content for image viewing

--- src/image_viewer.py
+++ src/image_viewer.py
@@ -12,7 +12,7 @@ class Application(tk.Frame):
         super().__init__(master)
         self.pack()
 
-        self.master.geometry("1100x660")
+        self.master.geometry("1060x610")
         self.master.title("Annotator")
 
         self.canvas1 = None
@@ -25,42 +25,70 @@ class Application(tk.Frame):
         self.current_id = 0
         self.frame_num = None
         self.frame_data = {}
+        self.scale_var = tk.DoubleVar()
+        self.scale = None
+        self.slider = None
         self.create_widgets()
         self.canvas1.bind("<ButtonPress-1>", self.start_point_get)
         self.canvas1.bind("<Button1-Motion>", self.oval_drawing)
+        self.canvas1.bind("<Button2-Motion>", self.drag)
         self.canvas1.bind("<ButtonRelease-1>", self.release_action)
+        self.canvas1.bind("<KeyPress>", self.key_event)
+        self.canvas1.focus_set()
+
+    def scroll(self, event):
+        self.canvas1.focus_set()
+        self.current_id = int(self.scale_var.get())
+        print(f'===== FRAME {self.current_id} =====')
+        self.canvas1.create_image(483, 273, image=self.images[self.current_id])
+
+    def key_event(self, event):
+        key = event.keysym
+        if key == 'n':
+            self.next_image()
+        elif key == 'b':
+            self.before_image()
+        elif key == 's':
+            self.save()
+        elif key == 'l':
+            self.load_image()
 
     def create_widgets(self):
-        self.canvas1 = tk.Canvas(self)
-        self.canvas1.configure(width=960, height=540, bg='black')
-        self.canvas1.grid(column=1, row=0)
-        self.canvas1.grid(padx=20, pady=20)
+        side_panel = tk.Frame(self.master, borderwidth=2, relief=tk.SUNKEN)
+        open_button = ttk.Button(side_panel, text='load')
+        open_button.configure(command=self.load_image)
+        open_button.pack()
+        next_button = ttk.Button(side_panel, text='next')
+        next_button.configure(command=self.next_image)
+        next_button.pack()
+        before_button = ttk.Button(side_panel, text='before')
+        before_button.configure(command=self.before_image)
+        before_button.pack()
+        save_button = ttk.Button(side_panel, text='save')
+        save_button.configure(command=self.save)
+        save_button.pack()
+        side_panel.pack(side=tk.RIGHT, fill=tk.Y)
 
-        frame_button = ttk.LabelFrame(self)
-        frame_button.grid(column=1, row=1)
-        frame_button.grid(padx=0, pady=0)
+        self.slider = tk.Frame(self.master, borderwidth=2, relief=tk.SUNKEN)
+        self.scale = tk.Scale(self.slider,
+                              variable=self.scale_var,
+                              command=self.scroll,
+                              orient=tk.HORIZONTAL,
+                              length=930,
+                              width=20,
+                              sliderlength=10,
+                              from_=0,
+                              to=0,
+                              resolution=1,
+                              tickinterval=5)
+        self.scale.pack()
+        self.slider.pack(side=tk.BOTTOM, fill=tk.X)
 
-        button_open = ttk.Button(frame_button)
-        button_open.configure(text='load')
-        button_open.grid(column=0, row=1)
-        button_open.configure(command=self.load_image)
-
-        button_next = ttk.Button(frame_button)
-        button_next.config(text='next')
-        button_next.grid(column=1, row=1)
-        button_next.configure(command=self.next_image)
-
-        button_before = ttk.Button(frame_button)
-        button_before.config(text='before')
-        button_before.grid(column=2, row=1)
-        button_before.configure(command=self.before_image)
-
-        button_save = ttk.Button(frame_button)
-        button_save.config(text='save')
-        button_save.grid(column=3, row=1)
-        button_save.configure(command=self.save)
+        self.canvas1 = tk.Canvas(self.master)
+        self.canvas1.pack(expand=True, fill=tk.BOTH)
 
     def load_image(self):
+        self.canvas1.focus_set()
         self.current_id = 0
         filename = filedialog.askopenfilename()
         cap = cv2.VideoCapture(filename)
@@ -87,14 +115,29 @@ class Application(tk.Frame):
             'radius': None
         } for i in range(self.frame_num)}
         self.canvas1.create_image(483, 273, image=self.images[self.current_id])
+        self.scale.destroy()
+        self.scale = tk.Scale(self.slider,
+                              variable=self.scale_var,
+                              command=self.scroll,
+                              orient=tk.HORIZONTAL,
+                              length=930,
+                              width=20,
+                              sliderlength=10,
+                              from_=0,
+                              to=len(self.images) - 1,
+                              resolution=1,
+                              tickinterval=5)
+        self.scale.pack()
 
     def next_image(self):
+        self.canvas1.focus_set()
         if self.current_id < self.frame_num - 1:
             self.current_id += 1
         print(f'===== FRAME {self.current_id} =====')
         self.canvas1.create_image(483, 273, image=self.images[self.current_id])
 
     def before_image(self):
+        self.canvas1.focus_set()
         if self.current_id > 0:
             self.current_id -= 1
         print(f'===== FRAME {self.current_id} =====')
@@ -114,13 +157,6 @@ class Application(tk.Frame):
                     writer.writerow([data.get('frame_id'), data.get('x'), data.get('y'), data.get('radius')])
 
     def start_point_get(self, event):
-        if not (self.start_x is None or self.start_y is None or self.end_x is None or self.end_y is None):
-            is_in_before_rectangle = self.start_x <= event.x <= self.end_x and self.start_y <= event.y <= self.end_y
-            if is_in_before_rectangle:
-                # self.canvas1.bind('oval1', "<Button1-Motion>", self.drag)
-                self.drag(event)
-                return
-
         self.canvas1.delete("oval1")
         self.oval = self.canvas1.create_oval(event.x, event.y, event.x + 1, event.y + 1, outline="red", tag="oval1")
         self.start_x, self.start_y = event.x, event.y
@@ -139,14 +175,19 @@ class Application(tk.Frame):
         self.canvas1.coords("oval1", self.start_x, self.start_y, self.end_x, self.end_y)
 
     def drag(self, event):
-        self.start_x = event.x
-        self.start_y = event.y
-        print(self.oval)
+        diff_x = event.x - self.start_x
+        diff_y = event.y - self.start_y
         self.canvas1.move(
             self.oval,
-            event.x - self.start_x,
-            event.y - self.start_y
+            diff_x,
+            diff_y
         )
+        self.start_x = event.x
+        self.start_y = event.y
+        self.end_x += diff_x
+        self.end_y += diff_y
+        self.canvas1.coords('oval1', self.start_x, self.start_y, self.end_x, self.end_y)
+        self.release_action(event)
 
     def release_action(self, event):
         start_x, start_y, end_x, end_y = [
@@ -169,7 +210,6 @@ class Application(tk.Frame):
             'y': y_center,
             'radius': radius
         }
-        # self.next_image()
 
 
 if __name__ == "__main__":
